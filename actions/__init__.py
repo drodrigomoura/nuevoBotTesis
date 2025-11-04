7eff289df4ec0c765056ba12a4ee57c819b14dc3
Damian Rodrigo Moura <d.rodrigomoura@gmail.com>
2025-11-03 22:45:37 -0300

    chore: changes in __init__.py

--- actions/__init__.py
+++ actions/__init__.py
@@ -1,13 +1,4 @@
 # Import all actions so they are registered with Rasa
-from actions.actions import ActionAuthenticateUser, ActionConsultarAsistencia, ActionConsultarFechasParciales
-from actions.actionsMaterias import ActionConsultarMaterias, ActionConsultarNotas, ActionConsultarRequerimientosMateria
+from actions.actions import *
+from actions.actionsMaterias import *
 from actions.actionsMesaExamen import *
-
-__all__ = [
-    'ActionAuthenticateUser',
-    'ActionConsultarAsistencia',
-    'ActionConsultarFechasParciales',
-    'ActionConsultarMaterias',
-    'ActionConsultarNotas',
-    'ActionConsultarRequerimientosMateria',
-]
